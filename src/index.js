33c52b7d8c9e37396edcc647c1cab80ebf5930f3
Courtney Silva <csilva@salientprocess.com>
2022-12-07 10:56:36 -0800

    feat(tutorial): complete step 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +11,10 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
